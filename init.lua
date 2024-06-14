7b7ae744b69a85fbef063d9af174e913003bbd88
anusikh <anusikh2001@gmail.com>
2024-06-14 23:52:42 +0530

    splitting code

--- init.lua
+++ init.lua
@@ -19,30 +19,7 @@ Plug('hrsh7th/nvim-cmp')
 Plug('mfussenegger/nvim-jdtls')
 vim.call("plug#end")
 
-local keyset = vim.keymap.set
-
--- for trouble
-if pcall(require, 'trouble') then require('trouble').setup() end
-
--- for Comment
-if pcall(require, 'Comment') then require('Comment').setup() end
-
--- for nerdtree
-keyset('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true })
-keyset('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
-keyset('n', '<C-n>', ':NERDTree<CR>', { noremap = true })
-keyset('n', '<C-f>', ':NERDTreeFind<CR>', { noremap = true })
-
--- for telescope
-if pcall(require, 'telescope.builtin')
-then
-  local builtin = require('telescope.builtin')
-  keyset('n', '<C-p>', builtin.find_files, {})
-  keyset('n', '<C-r>', builtin.live_grep, {})
-  keyset('n', '<leader>fb', builtin.buffers, {})
-  keyset('n', '<leader>fh', builtin.help_tags, {})
-end
-
 require("options")
+require("plugins")
 require("lsp")
 require("ndap")
---
+++ lua/plugins.lua
@@ -0,0 +1,24 @@
+local vim = vim
+local keyset = vim.keymap.set
+
+-- for trouble
+if pcall(require, 'trouble') then require('trouble').setup() end
+
+-- for Comment
+if pcall(require, 'Comment') then require('Comment').setup() end
+
+-- for nerdtree
+keyset('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true })
+keyset('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
+keyset('n', '<C-n>', ':NERDTree<CR>', { noremap = true })
+keyset('n', '<C-f>', ':NERDTreeFind<CR>', { noremap = true })
+
+-- for telescope
+if pcall(require, 'telescope.builtin')
+then
+  local builtin = require('telescope.builtin')
+  keyset('n', '<C-p>', builtin.find_files, {})
+  keyset('n', '<C-r>', builtin.live_grep, {})
+  keyset('n', '<leader>fb', builtin.buffers, {})
+  keyset('n', '<leader>fh', builtin.help_tags, {})
+end
