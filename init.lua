3c31f7ea51e0a693106c52c832ab1eb270994108
anusikh <anusikh2001@gmail.com>
2024-06-17 03:48:03 +0530

    migrate plug -> lazy.nvim

--- init.lua
+++ init.lua
@@ -1,24 +1,19 @@
 local vim = vim
-local Plug = vim.fn['plug#']
 
-vim.call("plug#begin")
-Plug('preservim/nerdtree', { ['on'] = 'NERDTreeToggle' })
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim')
-Plug('numToStr/Comment.nvim')
-Plug('folke/trouble.nvim')
--- plugins for debugger
-Plug('mfussenegger/nvim-dap')
--- plugins for lsp and autocomplete
-Plug('williamboman/mason.nvim')
-Plug('neovim/nvim-lspconfig')
-Plug('hrsh7th/cmp-nvim-lsp')
-Plug('hrsh7th/nvim-cmp')
--- lsp for java
-Plug('mfussenegger/nvim-jdtls')
-vim.call("plug#end")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup("plugins")
 require("options")
-require("plugins")
 require("lsp")
 require("ndap")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,24 +1,43 @@
-local vim = vim
 local keyset = vim.keymap.set
 
--- for trouble
-if pcall(require, 'trouble') then require('trouble').setup() end
-
--- for Comment
-if pcall(require, 'comment') then require('comment').setup() end
-
--- for nerdtree
-keyset('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true })
-keyset('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
-keyset('n', '<C-n>', ':NERDTree<CR>', { noremap = true })
-keyset('n', '<C-f>', ':NERDTreeFind<CR>', { noremap = true })
-
--- for telescope
-if pcall(require, 'telescope.builtin')
-then
-  local builtin = require('telescope.builtin')
-  keyset('n', '<C-p>', builtin.find_files, {})
-  keyset('n', '<C-r>', builtin.live_grep, {})
-  keyset('n', '<leader>fb', builtin.buffers, {})
-  keyset('n', '<leader>fh', builtin.help_tags, {})
-end
+return {
+  {
+    "preservim/nerdtree",
+    config = function()
+      keyset('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true })
+      keyset('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
+      keyset('n', '<C-n>', ':NERDTree<CR>', { noremap = true })
+      keyset('n', '<C-f>', ':NERDTreeFind<CR>', { noremap = true })
+    end
+  },
+  "nvim-lua/plenary.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      local builtin = require('telescope.builtin')
+      keyset('n', '<C-p>', builtin.find_files, {})
+      keyset('n', '<C-r>', builtin.live_grep, {})
+      keyset('n', '<leader>fb', builtin.buffers, {})
+      keyset('n', '<leader>fh', builtin.help_tags, {})
+    end
+  },
+  {
+    'echasnovski/mini.comment',
+    version = '*',
+    config = function()
+      require('mini.comment').setup()
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    config = function()
+      require('trouble').setup()
+    end
+  },
+  "mfussenegger/nvim-dap",
+  "williamboman/mason.nvim",
+  "neovim/nvim-lspconfig",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/nvim-cmp",
+  "mfussenegger/nvim-jdtls",
+}
