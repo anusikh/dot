ef4b68639d720fadd31f2c03b751e04c557a0697
anusikh <anusikh.panda@autodesk.com>
2025-10-06 03:07:14 +0530

    revert changes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -12,10 +12,3 @@ vim.opt.writebackup = false
 vim.opt.updatetime = 300
 vim.opt.signcolumn = "yes"
 vim.api.nvim_set_option("clipboard", "unnamedplus")
-
-if vim.g.neovide then
-    vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-    vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-    vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
-end
-
